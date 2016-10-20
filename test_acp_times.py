14dc1988c3612064703d9686f55b574a91594004
brianLeeson <brianeleeson@gmail.com>
2016-10-20 15:37:22 -0700

    testing datetime

--- test_acp_times.py
+++ test_acp_times.py
@@ -2,11 +2,10 @@
 Nose Tests for project 4
 """
 
-from acp_times import open_time
-from acp_times import close_time
+from acp_times
 
 #import arrow
 
 def test_web_examples():
 	#assert open_time(200, 60, '2017-01-01 ' )
-	print(open_time())
+	print(acp_times.open_time())
